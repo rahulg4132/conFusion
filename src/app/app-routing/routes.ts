bd3702a79129527ecbeb98ec0db358bbe7251d34
Rahul Gupta <rahulg4132@gmail.com>
2020-06-26 13:21:37 +0530

    4th commit

--- src/app/app-routing/routes.ts
+++ src/app/app-routing/routes.ts
@@ -9,5 +9,7 @@ import { ContactComponent } from '../contact/contact.component';
 export const routes: Routes = [
     {path:'home', component: HomeComponent},
     {path: 'menu', component: MenuComponent},
-    {path: '', redirectTo: '/home', pathMatch: 'full'}
+    {path: 'dishdetail/:id', component: DishdetailComponent},
+    {path: 'contactus', component: ContactComponent},
+    {path: '', redirectTo: '/home', pathMatch: 'full'}    
 ];
